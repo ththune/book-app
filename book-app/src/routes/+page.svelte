4c796f40586144ce77cd9695dcd0dc14162faf8c
ththune <135433132+ththune@users.noreply.github.com>
2024-06-30 11:17:09 +0200

    Created skeleton for the home page, which also fetches a list of books onMount.

--- book-app/src/routes/+page.svelte
+++ book-app/src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+
+	let data;
+	let books;
+
+	async function getBooks() {
+		const response = await fetch('http://localhost:5086/Book', {
+			method: 'GET',
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		books = await response.json();
+	}
+
+	onMount(async () => {
+		await getBooks();
+	});
+</script>
+
+<h1>The Book App</h1>
+<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Home" />
+</svelte:head>
+
+<div class="text-column">
+	<!-- <h1>Get som data here from an API</h1> -->
+
+	<!-- <button on:click={getBooks}>Get books</button> -->
+	{#if books}
+		{#each books as book (book.bookId)}
+			<!-- <a class="how-to-play" href="/sverdle/how-to-play">How to play</a> -->
+
+			<a class="how-to-play" href="/get-data/test-folder">
+				<div class="row">
+					<p>{book.bookId}</p>
+					<p>{book.bookTitle}</p>
+					<p>{book.bookIsbn}</p>
+					<p>{book.bookPublishedDate}</p>
+				</div>
+			</a>
+		{/each}
+	{/if}
+</div>
+
+<style>
+	.row {
+		display: flex;
+		gap: 10px;
+	}
+
+	.row p {
+		margin: 0;
+	}
+</style>
