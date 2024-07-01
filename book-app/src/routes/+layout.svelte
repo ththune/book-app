85c29697f864964ec32c6fd4f8451d352d5aa76b
ththune <135433132+ththune@users.noreply.github.com>
2024-07-01 20:49:33 +0200

    Don't show <Header /> on the /login page

--- book-app/src/routes/+layout.svelte
+++ book-app/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 
 	import Header from './Header.svelte';
 	import './styles.css';
@@ -21,7 +22,9 @@
 </script>
 
 <div class="app">
-	<Header />
+	{#if $page.url.pathname !== '/login'}
+		<Header />
+	{/if}
 
 	<main>
 		{#if status in errorPages}
