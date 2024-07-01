3bddc4ad0a7489905db1087f2ee0281851dc60ed
ththune <135433132+ththune@users.noreply.github.com>
2024-07-02 00:38:59 +0200

    Better styling on the login page.

--- book-app/src/routes/login/+page.svelte
+++ book-app/src/routes/login/+page.svelte
@@ -24,19 +24,50 @@
 	}
 </script>
 
-<form on:submit={login}>
-	<div>
-		<label for="username">Username:</label>
-		<input type="username" id="username" bind:value={username} required />
-	</div>
-	<div>
-		<label for="password">Password:</label>
-		<input type="password" id="password" bind:value={password} required />
-	</div>
+<div class="container">
+	<form class="center-form" on:submit={login}>
+		<div>
+			<label for="username">Username:</label>
+			<input type="username" id="username" bind:value={username} required />
+		</div>
+		<div>
+			<label for="password">Password:</label>
+			<input type="password" id="password" bind:value={password} required />
+		</div>
 
-	<button type="submit">Login</button>
-</form>
+		<button class="submit-button" type="submit">Login</button>
 
-{#if errorMessage}
-	<p class="error">{errorMessage}</p>
-{/if}
+		{#if errorMessage}
+			<p class="error">{errorMessage}</p>
+		{/if}
+	</form>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 30vh;
+		width: 20vw;
+		max-width: 600px;
+		margin: auto auto;
+	}
+
+	.center-form {
+		background-color: #f2f2f2;
+		padding: 20px;
+		border-radius: 5px;
+		box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+	}
+
+	.center-form label {
+		display: block;
+		margin-bottom: 5px;
+	}
+
+	.submit-button {
+		display: block;
+		margin-top: 10px;
+	}
+</style>
